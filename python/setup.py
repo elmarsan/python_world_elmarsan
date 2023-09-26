a8788e096221dfb2fdf5f0e5d18656367b1fb0ea
elmarsan <mselias97@gmail.com>
2023-09-26 12:16:34 +0200

    update setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,3 +1,5 @@
+import setuptools
+
 setuptools.setup(
     name="python_world",
     version="0.0.1",
@@ -5,6 +7,7 @@ setuptools.setup(
     author_email="mselias97@gmail.com",
     description="A function that returns 'world'",
     url="https://github.com/elmarsan/python_world_elmarsan",
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
